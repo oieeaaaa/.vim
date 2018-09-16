1fdf2bd4a514e90527f59a0c488e7bcece438628
Leonard Dizon <dizon.leonard@gmail.com>
2018-09-16 22:18:56 +0800

    Fix helptags creation

--- setup-plugins.sh
+++ setup-plugins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PLUGIN_DIR=~/.vim/pack/plugins/start
 printf "Generating plugin directory...\n"
-mkdir -p ~/.vim/pack/plugins/start
-cd ~/.vim/pack/plugins/start
+mkdir -p $PLUGIN_DIR
+cd $PLUGIN_DIR
 
 # download plugins
 printf "Downloading plugin files from Github\n\n"
@@ -9,4 +10,4 @@ git clone https://github.com/leafgarland/typescript-vim #typscript syntax
 git clone https://github.com/vim-airline/vim-airline #airline status bar
 git clone https://github.com/vimwiki/vimwiki.git #vimwiki
 
-vim -c helptags ALL # setup helptags
+vim -c "helptags $PLUGIN_DIR" # setup helptags

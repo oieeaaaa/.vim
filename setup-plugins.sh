fe88e92e1e2a0814995ce37f5af20dc771d218a2
Leonard Dizon <leonard@snekbyte.com>
2019-05-08 14:27:52 +0800

    Show progress of plugin download
    Separated list of github links to avoid repetition of
    the `git clone` command. Also silenced the command's output.

--- setup-plugins.sh
+++ setup-plugins.sh
@@ -5,15 +5,27 @@ mkdir -p $PLUGIN_DIR
 cd $PLUGIN_DIR
 
 # download plugins
-printf "Downloading plugin files from Github\n\n"
-git clone https://github.com/leafgarland/typescript-vim #typscript syntax
-git clone https://github.com/vim-airline/vim-airline #airline status bar
-git clone https://github.com/vimwiki/vimwiki.git #vimwiki
+REPOS=(
+https://github.com/leafgarland/typescript-vim #typscript syntax
+https://github.com/vim-airline/vim-airline #airline status bar
+https://github.com/vimwiki/vimwiki.git #vimwiki
+https://github.com/pangloss/vim-javascript.git #js syntax highlighting
+https://github.com/mxw/vim-jsx.git #jsx support
+)
 
-printf "\nSetting up Help Tags...\n"
+PATTERN="s/https:\/\/[\.w]*github\.com\/.+\/([[:alnum:]-]+)(\.git)?/\1/"
+
+printf "Downloading plugin files from Github...\n"
+for i in ${!REPOS[@]}; do
+  echo "($(($i+1))/${#REPOS[@]}) $(sed -E $PATTERN <<< ${REPOS[$i]})"
+  git clone ${REPOS[$i]} > /dev/null 2>&1
+done
+
+printf "Setting up Help Tags...\n"
 PLUGINS=$(find * -maxdepth 0 -type d -printf '%p,')
-
 vim -u NONE -c "set runtimepath=${PLUGINS::-1}" -c "helptags ALL" -c q # setup helptags
+
+printf "\n"
 if [ $? -eq 0 ]; then
     printf "Setup completed successfully.\n"
 else
